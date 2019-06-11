9358d618488e473cafde2eca3cc9895b2ffcdb74
rambala99 <51699733+rambala99@users.noreply.github.com>
2019-06-11 14:51:20 +0530

    positivenegative.c

--- positivenegative.c
+++ positivenegative.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
 	int a;
 	scanf("%d",&a);
-	if(a>0){
+	if(a>0)
 	printf("Positive");
-	}else if(a<0)
+	else if(a<0)
 	{
 	printf("Negative");
 	}
