06ffa9c9f07d7273e0932eddd169438f70aa7cdb
rambala99 <51699733+rambala99@users.noreply.github.com>
2019-06-11 14:40:45 +0530

    positivenegative.c

---
+++ positivenegative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a>0){
+	printf("positive number");
+	}else if(a<0)
+	{
+	printf("negative number");
+}else
+printf("numer is zero");
+}
